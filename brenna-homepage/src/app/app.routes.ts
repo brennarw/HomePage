6e00327a8557b9f23ebd2b5561b3abdcece71410
Brenna Wolf <brennarw@colostate.edu>
2025-04-02 15:05:49 -0600

    started

---
+++ brenna-homepage/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { ProjectsComponent } from './projects/projects.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AboutComponent } from './about/about.component';
+
+export const routes: Routes = [
+    {path:'', component: HomepageComponent},
+    {path:'about', component: AboutComponent},
+    {path:'projects', component: ProjectsComponent},
+];
