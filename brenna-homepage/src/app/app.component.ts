6c18eb8d9db9c985b9b43f2b6e3430fa9f73c46a
Brenna Wolf <brennarw@colostate.edu>
2025-04-07 00:00:52 -0600

    more pages added

--- brenna-homepage/src/app/app.component.ts
+++ brenna-homepage/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   template: `
-    <app-navbar></app-navbar>
-    <router-outlet></router-outlet>
+    <div class="app-container">
+      <app-navbar></app-navbar>
+      <router-outlet></router-outlet>
+      <app-footer></app-footer>
+    </div>
 `,
   styleUrl: './app.component.css'
 })
--- brenna-homepage/src/app/app.routes.ts
+++ brenna-homepage/src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { ProjectsComponent } from './projects/projects.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { AboutComponent } from './about/about.component';
+import { ResearchComponent } from './research/research.component';
 
 export const routes: Routes = [
+    {path:'', component: HomepageComponent},
     {path:'home', component: HomepageComponent},
     {path:'about', component: AboutComponent},
     {path:'projects', component: ProjectsComponent},
+    {path:'research', component: ResearchComponent},
 ];
