d79d537155b560aa173f49082ca604e5ab203455
Brenna Wolf <brennarw@colostate.edu>
2025-04-21 17:34:58 -0600

    styling added to charts

--- brenna-homepage/src/app/charting/charting.component.ts
+++ brenna-homepage/src/app/charting/charting.component.ts
@@ -14,6 +14,9 @@ import * as d3 from 'd3';
 export class ChartingComponent {
   showPenguinCharts: boolean = false;
   showTrafficCharts: boolean = false;
+  showOlympicCharts: boolean = false;
+  showOlympicScatter: boolean = false;
+  showOlympicBar: boolean = false;
 
   //ViewChild is used to access the div in the DOM after view initialization => plotContainer is initialized after the browser has been loaded
   @ViewChild('plotContainer', { static: true }) plotContainer!: ElementRef;
@@ -23,8 +26,19 @@ export class ChartingComponent {
   //ObservablePlot is only used client-side, and therefore can't be rendered server-side. Since it uses DOM APIs, these aren't available until the browser is rendered
   
   togglePenguin() {
+    this.plotContainer.nativeElement.innerHTML = '';
     this.showPenguinCharts = !this.showPenguinCharts;
-    this.getPenguinCharts('flipper_length_mm', 'steelblue');
+    this.showOlympicCharts = false;
+    this.showTrafficCharts = false;
+    this.showOlympicBar = false;
+    this.showOlympicScatter = false;
+  }
+
+  toggleOlympics() {
+    this.plotContainer.nativeElement.innerHTML = '';
+    this.showOlympicCharts = !this.showOlympicCharts;
+    this.showPenguinCharts = false;
+    this.showTrafficCharts = false;
   }
 
   getPenguinCharts(y_label: string, color: string) {
@@ -37,7 +51,16 @@ export class ChartingComponent {
             const plot = Plot.plot({
               color: {legend: true},
               marks: [
-                Plot.rectY(penguinData, Plot.binX({y2: "count"}, {x: y_label, fill: "species"} as any)),  
+                Plot.rectY(penguinData, 
+                  Plot.binX(
+                    {
+                      y2: "count"
+                    }, 
+                    {
+                      x: y_label, 
+                      fill: "species"
+                    } as any
+                  )),  
               ],
               height: 300,
               width: 500
@@ -53,7 +76,11 @@ export class ChartingComponent {
 
   getTrafficCharts() {
     this.showTrafficCharts = !this.showTrafficCharts;
-    console.log(this.showTrafficCharts);
+    this.showOlympicCharts = false;
+    this.showPenguinCharts = false;
+    this.showOlympicBar = false;
+    this.showOlympicScatter = false;
+    this.plotContainer.nativeElement.innerHTML = '';
     if (this.showTrafficCharts) {
       if (isPlatformBrowser(this.platformId)){
         fetch('data/traffic_weekly.csv')
@@ -95,5 +122,74 @@ export class ChartingComponent {
      }
   }
   
+  getOlympicScatterChart() {
+    this.showOlympicScatter = !this.showOlympicScatter;
+    this.showOlympicBar = false;
+    if (this.showOlympicCharts && this.showOlympicScatter) {
+      if (isPlatformBrowser(this.platformId)){
+        this.plotContainer.nativeElement.innerHTML = '';
+        fetch('data/athletes.csv')
+            .then(res => res.text())
+            .then(data => {
+              const olympicData = d3.csvParse(data, d => ({
+                name: d['name'],
+                nationality: d['nationality'],
+                height: +d['height'],
+                weight: +d['weight'],
+                sport: d['sport'],
+                sex: d['sex'],
+              }));
+              console.log(olympicData);
+              const plot = Plot.dot(olympicData, {
+                x: "weight",
+                y: "height",
+                stroke: "sex",
+                channels: {name: "name", sport: "sport"},
+                tip: true
+              }).plot()
+              
+              this.plotContainer.nativeElement.appendChild(plot);
+            }); 
+      }
+    } else {
+      this.plotContainer.nativeElement.innerHTML = '';
+    }
+  }
+
+  getOlympicBarChart() {
+    this.showOlympicBar = !this.showOlympicBar;
+    this.showOlympicScatter = false;
+    if (this.showOlympicCharts && this.showOlympicBar) {
+      if (isPlatformBrowser(this.platformId)){
+        this.plotContainer.nativeElement.innerHTML = '';
+        fetch('data/athletes.csv')
+            .then(res => res.text())
+            .then(data => {
+              const olympicData = d3.csvParse(data, d => ({
+                name: d['name'],
+                nationality: d['nationality'],
+                height: +d['height'],
+                weight: +d['weight'],
+                sport: d['sport'],
+                sex: d['sex'],
+              }));
+              const plot = Plot.plot({
+                fx: {padding: 0, label: null, tickRotate: 90, tickSize: 6},
+                x: {axis: null},
+                y: {grid: true},
+                color: {legend: true},
+                marks: [
+                  Plot.barY(olympicData, Plot.groupX({y2: "count"}, {x: "sex", fx: "sport", fill: "sex"})),
+                  Plot.ruleY([0])
+                ]
+              })
+              
+              this.plotContainer.nativeElement.appendChild(plot);
+            }); 
+      }
+    } else {
+      this.plotContainer.nativeElement.innerHTML = '';
+    }
+  }
 
 }
